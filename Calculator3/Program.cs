75aa10d68cb62757e0ceb11710d195a1b4983bc4
nasirisara <99367926+nasirisara@users.noreply.github.com>
2022-02-10 23:41:40 +0100

    Add project files.

---
+++ Calculator3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Calculator3
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            string strnum1, opera, strnum2;
+           strnum1= Console.ReadLine();
+            opera = Console.ReadLine();
+            strnum2 = Console.ReadLine();
+            double num1, num2;
+            num1 = double.Parse(strnum1);
+            num2 = double.Parse(strnum2);
+
+            switch (opera)
+            {
+                case "+": Console.WriteLine(num1 + num2);
+                    break;
+
+                case "-": Console.WriteLine(num1 - num2);
+                    break;
+
+                case "/": Console.WriteLine(num1 / num2);
+                    break;
+
+                case "*": Console.WriteLine(num1 * num2);
+                    break;
+                    
+            }
+
+
+            Console.ReadKey();
+
+
+
+           
+        }
+    }
+}
